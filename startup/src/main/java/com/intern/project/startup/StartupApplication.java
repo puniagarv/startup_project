85a469756d1f45bcff54fe704246f678a1a41818
puniagarv <puniagaurav14@gmail.com>
2025-03-02 19:42:37 +0530

    final commit

--- startup/src/main/java/com/intern/project/startup/StartupApplication.java
+++ startup/src/main/java/com/intern/project/startup/StartupApplication.java
@@ -1,5 +1,6 @@
 package com.intern.project.startup;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartupApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(StartupApplication.class, args);
 	}
-
 }
